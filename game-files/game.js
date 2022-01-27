015b498677935d2aa15f56230fc3139b176a6b5a
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-01-26 21:02:26 -0500

    Update game.js

--- game-files/game.js
+++ game-files/game.js
@@ -10,4 +10,105 @@ let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
+let questions = [
+    {
+        question: 'What is 2 + 2',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        answer: 1,
+    },
+    {
+        question: 'What is 2 + 2',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        answer: 1,
+    },
+    {
+        question: 'What is 2 + 2',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        answer: 1,
+    },
+    {
+        question: 'What is 2 + 2',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        choice1: '1',
+        answer: 1,
+    }
+]
 
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random()  * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset [ 'number']
+        choice.innerText = currentQuestion['choice' + number]
+
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct'  :
+        'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classlist.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classlist.remove(classToApply)
+            getNewQuestion()
+        
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
